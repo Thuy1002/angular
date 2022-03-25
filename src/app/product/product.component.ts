cedee5a7e462119e3bca67982ac68b8ff51cd1a1
thuy1002 <thuy1002dangthanh@gmail.com>
2022-03-25 23:07:45 +0700

    update code

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,24 +1,55 @@
 import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../services/product.service';
+
+
+type PRODUCT_TYPE = {
+  id: number,
+  name: string,
+  desc: string,
+  price: number
+};
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  product: any;
+  constructor(private ps: ProductService) { }
 
   ngOnInit(): void {
+    this.onGetlist();
   }
-  listProduct = new Array();
+  onGetlist(){
+    this.ps.getProducts().subscribe(data => {
+      this.product = data;
+    })
+  }
   newProduct = {
     id: 0,
     name: '',
     price: 0,
     desc: ''
   };
+
+
+  onDelete(id: string|number){
+    if(id){
+      this.ps.deleteProduct(id).subscribe(data => {
+        this.onGetlist();
+      });
+    }
+  }
+
+  /*  */
+  listProduct = new Array();
+  /*  */
+
+
+
+  /*  */
   Product_update = {
     id: 0,
     name: '',
@@ -29,6 +60,8 @@ export class ProductComponent implements OnInit {
     this.Product_update = p;
     console.log(p)
   }
+
+  /*  */
   onSubmit(product: any) {
     if (this.Product_update.id) {
       for (let i = 0; i < this.listProduct.length; i++) {
@@ -66,6 +99,7 @@ export class ProductComponent implements OnInit {
         desc: ''
       }
     }
-
+    /*  */
+    console.log(product);
   }
 }
